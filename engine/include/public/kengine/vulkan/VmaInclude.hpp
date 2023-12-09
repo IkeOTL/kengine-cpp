01aefc18c34c509f9443cc2b3e0acfea62608a57
Ike <iyousuf@kudodev.com>
2023-12-09 15:48:12 -0500

    fixed up cmake includes, working on descriptorsets

--- engine/include/public/kengine/vulkan/VmaInclude.hpp
+++ engine/include/public/kengine/vulkan/VmaInclude.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "VulkanInclude.hpp"
+#include <kengine/vulkan/VulkanInclude.hpp>
 
 #define VMA_VULKAN_VERSION 1003000
 #define VMA_STATIC_VULKAN_FUNCTIONS 0
 #define VMA_DYNAMIC_VULKAN_FUNCTIONS 0
-#include "vk_mem_alloc.h"
+#include <vma/vk_mem_alloc.h>
--- engine/include/public/kengine/vulkan/descriptor/DescriptorSetLayout.hpp
+++ engine/include/public/kengine/vulkan/descriptor/DescriptorSetLayout.hpp
@@ -1,28 +1,68 @@
 #pragma once
 #include <kengine/vulkan/VulkanInclude.hpp>
 #include <mutex>
+#include <vector>
+#include <unordered_map>
 
+namespace DescriptorSet {
+    struct DescriptorSetLayoutBindingConfig {
+    public:
+        const size_t bindingIndex;
+        const size_t descriptorCount;
+        const int descriptorType;
+        const int stageFlags;
 
-class DescriptorSetLayoutBindingConfig {
+        size_t hashCode() const {
+            size_t hash = 7;
+            hash = 41 * hash + bindingIndex;
+            hash = 41 * hash + descriptorCount;
+            hash = 41 * hash + descriptorType;
+            hash = 41 * hash + stageFlags;
+            return hash;
+        }
+    };
 
-private:
+    struct DescriptorSetLayoutConfig {
+    public:
+        const std::vector<DescriptorSetLayoutBindingConfig> bindings;
 
-public:
+        DescriptorSetLayoutConfig(std::initializer_list<DescriptorSetLayoutBindingConfig> b)
+            : bindings(b) { }
 
-};
+        const DescriptorSetLayoutBindingConfig& getBinding(size_t idx) const {
+            return bindings[idx];
+        }
 
-class DescriptorSetLayoutConfig {
+        size_t hashCode() const {
+            size_t hash = 7;
+            for (const auto& item : bindings)
+                hash = 53 * hash + item.hashCode();
+            return hash;
+        }
+    };
 
-private:
+    class DescriptorSetLayoutCache {
+    private:
 
-public:
+    public:
 
-};
+    };
+}
 
-class DescriptorSetLayoutCache {
+namespace std {
+    using namespace DescriptorSet;
 
-private:
+    template <>
+    struct hash<DescriptorSetLayoutConfig> {
+        size_t operator()(const DescriptorSetLayoutConfig& obj) const {
+            return obj.hashCode();
+        }
+    };
 
-public:
-
-};
+    template <>
+    struct hash<DescriptorSetLayoutBindingConfig> {
+        size_t operator()(const DescriptorSetLayoutBindingConfig& obj) const {
+            return obj.hashCode();
+        }
+    };
+}
