d41d7c20e0afca7aa4dfa00da0f53f5920a60390
Ike <iyousuf@kudodev.com>
2024-02-16 22:12:30 -0500

    WIP

--- engine/src/vulkan/renderpass/CascadeShadowMapRenderPass.cpp
+++ engine/src/vulkan/renderpass/CascadeShadowMapRenderPass.cpp
@@ -19,7 +19,7 @@ void CascadeShadowMapRenderPass::init(VulkanContext& vkCtx) {
 
 void CascadeShadowMapRenderPass::createRenderTargets(VmaAllocator vmaAllocator, const std::vector<VkImageView> sharedImageViews, const glm::uvec2 extents) {
     setDepthStencil(createDepthStencil(vmaAllocator, extents));
-    for (size_t i = 0; i < VulkanContext::FRAME_OVERLAP; i++) {
+    for (size_t i = 0; i < ShadowCascadeData::SHADOW_CASCADE_COUNT; i++) {
         auto fb = std::make_unique<CascadeShadowMapRenderTarget>(vkDevice, *getDepthStencil(), i);
         fb->init(*this, vmaAllocator, {}, extents);
         addRenderTarget(std::move(fb));
@@ -42,6 +42,56 @@ VkRenderPass CascadeShadowMapRenderPass::createVkRenderPass() {
 }
 
 std::unique_ptr<GpuImageView> CascadeShadowMapRenderPass::createDepthStencil(VmaAllocator vmaAllocator, const glm::uvec2 extents) {
-    lol
-        return nullptr;
+    VkImageCreateInfo imageCreateInfo{};
+    imageCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    imageCreateInfo.imageType = VK_IMAGE_TYPE_2D;
+    imageCreateInfo.mipLevels = 1;
+    imageCreateInfo.arrayLayers = ShadowCascadeData::SHADOW_CASCADE_COUNT;
+    imageCreateInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+    imageCreateInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+    imageCreateInfo.format = colorFormatAndSpace.getDepthFormat();
+    imageCreateInfo.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+    imageCreateInfo.extent.width = extents.x;
+    imageCreateInfo.extent.height = extents.y;
+    imageCreateInfo.extent.depth = 1;
+
+    VmaAllocationCreateInfo allocCreateInfo{};
+    allocCreateInfo.usage = VMA_MEMORY_USAGE_AUTO_PREFER_DEVICE;
+    allocCreateInfo.requiredFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+
+    VkImage vkImage;
+    VmaAllocation vmaImageAllocation;
+    VmaAllocationInfo allocationInfo;
+
+    VKCHECK(vmaCreateImage(vmaAllocator, &imageCreateInfo, &allocCreateInfo, &vkImage, &vmaImageAllocation, &allocationInfo),
+        "Failed to create depth-stencil image");
+
+    auto depthImage = std::make_shared<GpuImage>(GpuImage{
+            getVkDevice(),
+            vmaAllocator,
+            vkImage,
+            vmaImageAllocation,
+            imageCreateInfo
+        });
+
+    VkImageViewCreateInfo depthViewCreateInfo{};
+    depthViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+    depthViewCreateInfo.image = vkImage;
+    depthViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D_ARRAY;
+    depthViewCreateInfo.format = colorFormatAndSpace.getDepthFormat();
+    depthViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+    depthViewCreateInfo.subresourceRange.baseMipLevel = 0;
+    depthViewCreateInfo.subresourceRange.levelCount = 1;
+    depthViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+    depthViewCreateInfo.subresourceRange.layerCount = ShadowCascadeData::SHADOW_CASCADE_COUNT;
+
+    VkImageView depthImageView;
+    VKCHECK(vkCreateImageView(vkDevice, &depthViewCreateInfo, nullptr, &depthImageView),
+        "Failed to create depth-stencil image view");
+
+    return std::make_unique<GpuImageView>(GpuImageView{
+            depthImage,
+            depthImageView,
+            depthViewCreateInfo
+        });
 }
