cd98e2f23f6fbfa6d9f524b770e511e92a8fac4a
Ike <iyousuf@kudodev.com>
2024-02-18 16:43:14 -0500

    WIP

--- examples/src/Main.cpp
+++ examples/src/Main.cpp
@@ -2,6 +2,12 @@
 #include <glm/glm.hpp>
 #include <kengine/vulkan/renderpass/DeferredPbrRenderPass.hpp>
 #include <kengine/math.hpp>
+#include <kengine/vulkan/pipelines/DeferredOffscreenPbrPipeline.hpp>
+#include <kengine/vulkan/pipelines/SkinnedOffscreenPbrPipeline.hpp>
+#include <kengine/vulkan/pipelines/DeferredCompositionPbrPipeline.hpp>
+#include <kengine/vulkan/pipelines/CascadeShadowMapPipeline.hpp>
+#include <kengine/vulkan/pipelines/SkinnedCascadeShadowMapPipeline.hpp>
+#include <kengine/vulkan/pipelines/DrawCullingPipeline.hpp>
 
 int main() {
     glm::mat4 Proj = glm::mat4();
@@ -22,6 +28,35 @@ int main() {
             passes.emplace_back(std::move(rp));
 
             return passes;
+        },
+        [](VulkanContext& vkCtx, std::vector<std::unique_ptr<RenderPass>>& rp) {
+            auto pc = std::make_unique<PipelineCache>();
+
+            auto pass0 = std::make_unique<DeferredOffscreenPbrPipeline>();
+            pass0->init(vkCtx, rp[0].get(), vkCtx.getDescSetLayoutCache(), glm::vec2{});
+            pc->addPipeline(std::move(pass0));
+
+            auto skinned = std::make_unique<SkinnedOffscreenPbrPipeline>();
+            skinned->init(vkCtx, rp[0].get(), vkCtx.getDescSetLayoutCache(), glm::vec2{});
+            pc->addPipeline(std::move(skinned));
+
+            auto pass1 = std::make_unique<DeferredCompositionPbrPipeline>();
+            pass1->init(vkCtx, rp[0].get(), vkCtx.getDescSetLayoutCache(), glm::vec2{});
+            pc->addPipeline(std::move(pass1));
+
+            auto shadowPass = std::make_unique<CascadeShadowMapPipeline>();
+            shadowPass->init(vkCtx, rp[1].get(), vkCtx.getDescSetLayoutCache(), glm::vec2{ 4096 , 4096 });
+            pc->addPipeline(std::move(shadowPass));
+
+            auto skinnedShadowPass = std::make_unique<SkinnedCascadeShadowMapPipeline>();
+            skinnedShadowPass->init(vkCtx, rp[1].get(), vkCtx.getDescSetLayoutCache(), glm::vec2{ 4096 , 4096 });
+            pc->addPipeline(std::move(skinnedShadowPass));
+
+            auto culling = std::make_unique<DrawCullingPipeline>();
+            culling->init(vkCtx, nullptr, vkCtx.getDescSetLayoutCache(), glm::vec2{});
+            pc->addPipeline(std::move(culling));
+
+            return pc;
         },
         [](VulkanContext& vkCxt, Swapchain& swapchain, std::vector<std::unique_ptr<RenderPass>>& renderPasses) {
             std::vector<std::unique_ptr<RenderPass>> passes;
