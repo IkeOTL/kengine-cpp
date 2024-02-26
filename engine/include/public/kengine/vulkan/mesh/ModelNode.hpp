b99acc929cd7d4d003212d18b7574d978e885697
Ike <iyousuf@kudodev.com>
2024-02-25 23:14:10 -0500

    GLTF WOP

--- engine/include/public/kengine/vulkan/mesh/ModelNode.hpp
+++ engine/include/public/kengine/vulkan/mesh/ModelNode.hpp
@@ -1,4 +1,49 @@
 #pragma once
+#include <kengine/vulkan/mesh/ModelNode.hpp>
+#include <kengine/vulkan/mesh/Mesh.hpp>
+#include <kengine/Spatial.hpp>
+#include <memory>
 
-class ModelNode {
+class ModelNode;
+
+class ModelMesh {
+private:
+    ModelNode* parent = nullptr;
+    const std::unique_ptr<Mesh> mesh;
+
+public:
+    ModelMesh(std::unique_ptr<Mesh>&& mesh)
+        : mesh(std::move(mesh)) {};
+
+    const Mesh& getMesh() const {
+        if (!mesh)
+            throw std::runtime_error("Mesh was never set.");
+
+        return *mesh;
+    }
+
+    const ModelNode* getParent() const {
+        return parent;
+    }
+
+    void setParent(ModelNode* parent) {
+        this->parent = parent;
+    }
+};
+
+class ModelNode : public Spatial {
+private:
+    std::vector<std::unique_ptr<ModelMesh>> meshes;
+
+public:
+    ModelNode(std::string name) : Spatial(name) {}
+
+    void addMesh(std::unique_ptr<ModelMesh>&& m) {
+        m->setParent(this);
+        meshes.push_back(std::move(m));
+    }
+
+    const std::vector<std::unique_ptr<ModelMesh>>& getMeshes() const {
+        return meshes;
+    }
 };
--- engine/src/vulkan/mesh/ModelMesh.cpp
+++
@@ -1,2 +0,0 @@
-#include <kengine/vulkan/mesh/ModelMesh.hpp>
-
