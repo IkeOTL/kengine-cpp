4d9e1a175d47f0e5bdcf9747a34ed7b2cbf2e7da
Ike <iyousuf@kudodev.com>
2024-03-02 18:57:27 -0500

    working on asset cache WIP

--- engine/include/public/kengine/vulkan/AsyncAssetCache.hpp
+++ engine/include/public/kengine/vulkan/AsyncAssetCache.hpp
@@ -2,15 +2,17 @@
 #include <kengine/ExecutorService.hpp>
 #include <unordered_map>
 #include <functional>
+#include <shared_mutex>
 
 template <typename V, typename K>
 class AsyncAssetCache {
 private:
     ExecutorService& workerPool;
 
+    // maybe change to shared pointers? profile later
     std::unordered_map<size_t, std::unique_ptr<V>> cache;
     std::unordered_map<size_t, std::shared_future<V*>> backgroundTasks;
-    std::mutex lock;
+    std::shared_mutex lock;
 
 protected:
     virtual std::unique_ptr<V> create(std::shared_ptr<K> keyObj) = 0;
@@ -39,31 +41,117 @@ public:
         return res;
     }
 
-    std::shared_future<V*> get(std::shared_ptr<K> keyObj) {
-        std::lock_guard<std::mutex> lock(this->lock);
+    V& get(std::shared_ptr<K> keyObj) {
         size_t key = keyObj->hashCode();
-        auto cacheIt = cache.find(key);
-        if (cacheIt != cache.end())
-            return std::async(std::launch::deferred, [asset = cacheIt->second.get()]() { return asset; });
 
-        auto taskIt = backgroundTasks.find(key);
-        if (taskIt != backgroundTasks.end())
-            return taskIt->second;
+        // quick check with shared read lock
+        {
+            std::shared_lock<std::shared_mutex> lock(this->lock);
 
-        auto task = workerPool.submitShared([this, key, keyObj]() {
+            // already loaded
+            auto cacheIt = cache.find(key);
+            if (cacheIt != cache.end())
+                return *(cacheIt->second.get());
+
+            // currently loading
+            auto taskIt = backgroundTasks.find(key);
+            if (taskIt != backgroundTasks.end()) {
+                auto task = taskIt->second;
+                lock.unlock(); // dont deadlock, the task is expected to acquire a write lock
+                return *(taskIt->second.get());
+            }
+        }
+
+        // write
+        {
+            std::unique_lock<std::shared_mutex> lock(this->lock);
+
+            // already loaded
+            auto cacheIt = cache.find(key);
+            if (cacheIt != cache.end())
+                return *(cacheIt->second.get());
+
+            // double check
+            auto taskIt = backgroundTasks.find(key);
+            if (taskIt != backgroundTasks.end()) {
+                auto task = taskIt->second;
+                lock.unlock(); // dont deadlock, the task is expected to acquire a write lock
+                return *(taskIt->second.get());
+            }
+
+            std::promise<V*> promise;
+
+            backgroundTasks[key] = promise.get_future().share();
+
+            lock.unlock();
             auto asset = this->create(keyObj);
 
             {
-                std::lock_guard<std::mutex> lock(this->lock);
+                lock.lock();
                 auto& storedAsset = this->cache[key] = std::move(asset);
+                promise.set_value(storedAsset.get());
                 this->backgroundTasks.erase(key);
-
-                return storedAsset.get();
             }
-            });
+
+            return *(storedAsset.get());
+        }
+    }
+
+    // possible improvement: have multiple semaphore for the different phases so we dont block the entire section
+    std::shared_future<V*> getAsync(std::shared_ptr<K> keyObj) {
+        size_t key = keyObj->hashCode();
+
+        // quick check with shared read lock
+        {
+            std::shared_lock<std::shared_mutex> lock(this->lock);
+
+            // already loaded
+            auto cacheIt = cache.find(key);
+            if (cacheIt != cache.end()) {
+                std::promise<V*> promise;
+                promise.set_value(cacheIt->second.get());
+                return promise.get_future().share();
+            }
+
+            // currently loading
+            auto taskIt = backgroundTasks.find(key);
+            if (taskIt != backgroundTasks.end())
+                return taskIt->second;
+        }
+
+        // write
+        {
+            std::lock_guard<std::shared_mutex> lock(this->lock);
+
+            // already loaded
+            auto cacheIt = cache.find(key);
+            if (cacheIt != cache.end()) {
+                std::promise<V*> promise;
+                promise.set_value(cacheIt->second.get());
+                return promise.get_future().share();
+            }
+
+            // double check
+            auto taskIt = backgroundTasks.find(key);
+            if (taskIt != backgroundTasks.end())
+                return taskIt->second;
+
+            auto task = workerPool.submitShared([this, key, keyObj]() {
+                // need to handle exception to propagate to caller
+                auto asset = this->create(keyObj);
+
+                {
+                    std::lock_guard<std::shared_mutex> lock(this->lock);
+                    auto& storedAsset = this->cache[key] = std::move(asset);
+                    this->backgroundTasks.erase(key);
+
+                    return storedAsset.get();
+                }
+                });
 
             backgroundTasks[key] = task;
 
             return task;
+        }
     }
 };
--- engine/src/vulkan/texture/AsyncTextureCache.cpp
+++ engine/src/vulkan/texture/AsyncTextureCache.cpp
@@ -1,8 +1,8 @@
 #include <kengine/vulkan/texture/AsyncTextureCache.hpp>
 
-std::shared_future<Texture2d*> AsyncTextureCache::get(std::string key) {
+std::shared_future<Texture2d*> AsyncTextureCache::getAsync(std::string key) {
     auto ptr = std::make_shared<TextureConfig>(key);
-    return AsyncAssetCache::get(ptr);
+    return AsyncAssetCache::getAsync(ptr);
 }
 
 std::unique_ptr<Texture2d> AsyncTextureCache::create(std::shared_ptr<TextureConfig> keyObj) {
