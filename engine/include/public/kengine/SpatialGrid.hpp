bef20ee6f1c749718d5c274e4b1cc834a7de37f1
Ike <iyousuf@kudodev.com>
2024-03-22 23:04:34 -0400

    wip

--- engine/include/public/kengine/SpatialGrid.hpp
+++ engine/include/public/kengine/SpatialGrid.hpp
@@ -1,15 +1,18 @@
 #pragma once
 #include <thirdparty/entt.hpp>
+#include <glm/glm.hpp>
 
 class SpatialGrid {
 private:
     const int worldWidth, worldLength, cellSize;
-    const  int cellCountX, cellCountZ;
+    const int cellCountX, cellCountZ;
     const int worldOffsetX, worldOffsetZ;
 
     std::unordered_map<entt::entity, std::vector<entt::entity>*> entityIndex;
     std::vector<std::vector<entt::entity>> cells;
-    std::unordered_set < entt::entity> dirtySet;
-public:
+    std::unordered_set<entt::entity> dirtySet;
 
+public:
+    void getVisible(glm::vec3 camPos, std::vector<glm::vec3> frustomPoints,
+        FrustumIntersection frustumTester, std::vector<entt::entity> dest);
 };
