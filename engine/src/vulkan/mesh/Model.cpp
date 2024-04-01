ddc2cfe202ebf7a407ee6c585ebebb91bbd5386e
Ike <iyousuf@kudodev.com>
2024-03-31 23:41:14 -0400

    WIP

--- engine/src/vulkan/mesh/Model.cpp
+++ engine/src/vulkan/mesh/Model.cpp
@@ -31,18 +31,34 @@ Model::Model(std::vector<std::shared_ptr<Spatial>>&& nodes,
     rootNode = std::make_shared<ModelNode>("Main Node");
     fillRoot(rootNode, this->nodes);
 
-     calc bounds
+    // calc bounds
+    {
+        glm::vec3 min(std::numeric_limits<float>::infinity());
+        glm::vec3 max(-std::numeric_limits<float>::infinity());
+
+        glm::vec3 t0;
+        glm::vec3 t1;
+        for (auto& meshGroup : meshGroups) {
+            for (auto& mesh : meshGroup->getMeshes()) {
+                mesh->getBounds().getAabb().getMinMax(t0, t1);
+                min = glm::min(min, t0);
+                max = glm::min(max, t1);
+            }
+        }
+
+        bounds = Aabb::fromMinMax(min, max);
+    }
 }
 
-Model::Model(std::unique_ptr<Mesh>&& mesh){
+Model::Model(std::unique_ptr<Mesh>&& mesh) {
     rootNode = std::make_shared<ModelNode>("Main Node");
     nodes.push_back(rootNode);
+
+    bounds = mesh->getBounds();
+
     auto meshGroup = std::make_unique<MeshGroup>(0, 1);
     meshGroup->addMesh(std::move(mesh));
     meshGroups.push_back(std::move(meshGroup));
-
-
-    calc bounds
 }
 
 void Model::fillRoot(std::shared_ptr<Spatial> root, const std::vector<std::shared_ptr<Spatial>> nodes) {
