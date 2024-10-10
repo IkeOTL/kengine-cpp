278b2eca511b26d320379b3e16fb2c7239b30918
Sherisaac Yousuf <iyousuf@kudodev.com>
2024-10-10 18:06:58 -0400

    WIP

--- engine/include/public/kengine/EventBus.hpp
+++ engine/include/public/kengine/EventBus.hpp
@@ -6,62 +6,122 @@
 /// Inspired by https://github.com/libgdx/gdx-ai/
 
 enum TelegramReceiptMode {
-    RETURN_RECEIPT_UNNEEDED,
-    RETURN_RECEIPT_NEEDED,
-    RETURN_RECEIPT_SENT
+    RETURN_RECEIPT_UNNEEDED, RETURN_RECEIPT_NEEDED, RETURN_RECEIPT_SENT
+};
+
+enum TelegramDataBufSize {
+    NONE, TINY, SMALL, MEDIUM, LARGE
 };
 
 class Subscriber;
 
 struct Event {
 public:
+    eastl::weak_ptr<Subscriber> sender;
+    eastl::weak_ptr<Subscriber> recipient;
     uint32_t messageType;
-    eastl::weak_ptr<Event> sender;
-    eastl::weak_ptr<Event> recipient;
     TelegramReceiptMode returnReceiptStatus;
     float timestamp;
+    TelegramDataBufSize dataBufSize;
     void* data;
 };
 
 class Subscriber {
 public:
-    bool handleMessage(eastl::shared_ptr<Event> msg);
+    bool handleMessage(Event* msg);
 };
-
-constexpr uint32_t baseAlignment = 32;
-constexpr uint32_t telegramBlockSize = (sizeof(Event) + baseAlignment - 1) & ~(baseAlignment - 1);
 
 class EventPool
 {
 private:
-    eastl::fixed_allocator allocator;
-    unsigned char buf[1000 * telegramBlockSize];
+    unsigned char telegramBuf[1000 * sizeof(Event)];
+    eastl::fixed_allocator telegramAllocator;
+
+    unsigned char tinyDatabuf[1000 * 16];
+    eastl::fixed_allocator tinyAllocator;
+
+    unsigned char smallDatabuf[500 * 32];
+    eastl::fixed_allocator smallAllocator;
+
+    unsigned char mediumDataBuf[250 * 64];
+    eastl::fixed_allocator mediumAllocator;
+
+    unsigned char largeDataBuf[150 * 128];
+    eastl::fixed_allocator largeAllocator;
 
 public:
     EventPool()
     {
-        allocator.init(buf, sizeof(buf), telegramBlockSize, telegramBlockSize);
+        telegramAllocator.init(telegramBuf, sizeof(telegramBuf), sizeof(Event), alignof(Event));
+        tinyAllocator.init(tinyDatabuf, sizeof(tinyDatabuf), 16, 8);
+        smallAllocator.init(smallDatabuf, sizeof(smallDatabuf), 32, 8);
+        mediumAllocator.init(mediumDataBuf, sizeof(mediumDataBuf), 64, 8);
+        largeAllocator.init(largeDataBuf, sizeof(largeDataBuf), 128, 8);
     }
 
     ~EventPool() = default;
 
-    Event* rent() {
-        auto* mem = allocator.allocate(telegramBlockSize);
+    Event* rent(uint32_t bufSize = 0) {
+        auto* mem = telegramAllocator.allocate(sizeof(Event));
 
         if (!mem)
             throw std::bad_alloc();
 
-        auto* evt =  new (mem) Event();
+        memset(mem, 0, sizeof(Event));
+        auto* evt = new (mem) Event();
+
+        auto dataBufSize = TelegramDataBufSize::NONE;
+
+        if (bufSize > 64) {
+            dataBufSize = TelegramDataBufSize::LARGE;
+            evt->data = largeAllocator.allocate(128);
+        }
+        else if (bufSize > 32) {
+            dataBufSize = TelegramDataBufSize::MEDIUM;
+            evt->data = mediumAllocator.allocate(64);
+        }
+        else if (bufSize > 16) {
+            dataBufSize = TelegramDataBufSize::SMALL;
+            evt->data = smallAllocator.allocate(32);
+        }
+        else if (bufSize > 0) {
+            dataBufSize = TelegramDataBufSize::TINY;
+            evt->data = tinyAllocator.allocate(16);
+        }
+        else {
+            evt->dataBufSize = TelegramDataBufSize::NONE;
+            evt->data = nullptr;
+        }
+
+        evt->dataBufSize = dataBufSize;
+
 
         return evt;
     }
 
-    void release(Event* event) {
-        if (!event)
+    void release(Event* evt) {
+        if (!evt)
             return;
 
-        event->~Event();
-        allocator.deallocate(event, sizeof(Event));
+        evt->~Event();
+
+        // deallocate for data
+        switch (evt->dataBufSize) {
+        case TelegramDataBufSize::TINY:
+            tinyAllocator.deallocate(evt->data, 16);
+            break;
+        case TelegramDataBufSize::SMALL:
+            smallAllocator.deallocate(evt->data, 32);
+            break;
+        case TelegramDataBufSize::MEDIUM:
+            mediumAllocator.deallocate(evt->data, 64);
+            break;
+        case TelegramDataBufSize::LARGE:
+            largeAllocator.deallocate(evt->data, 128);
+            break;
+        }
+
+        telegramAllocator.deallocate(evt, sizeof(Event));
     }
 };
 
--- engine/tests/event_bus.cpp
+++ engine/tests/event_bus.cpp
@@ -10,20 +10,17 @@
 
 TEST_CASE("event-bus::EventBus. Basic", "[event-bus]") {
     EventPool pool;
-    {
-        auto* r0 = pool.rent();
-        r0->timestamp = 1337;
 
-        auto* r1 = pool.rent();
-        r1->timestamp = 1337;
+    auto* r0 = pool.rent();
+    r0->timestamp = 1337;
 
-        auto* r2 = pool.rent();
-        r2->timestamp = 1337;
+    auto* r1 = pool.rent();
+    r1->timestamp = 1337;
 
-        pool.release(r0);
-        pool.release(r1);
-        pool.release(r2);
-    }
-    int i = 0;
+    auto* r2 = pool.rent();
+    r2->timestamp = 1337;
 
+    pool.release(r0);
+    pool.release(r1);
+    pool.release(r2);
 }
